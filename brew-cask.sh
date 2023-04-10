be6aea59e6aab742e7e375a4655b1c3ada64324c
Aidan Loughney <36386493+ArLough@users.noreply.github.com>
2023-04-10 16:40:37 -0400

    Update brew-cask.sh
    Update to casks

--- brew-cask.sh
+++ brew-cask.sh
@@ -2,11 +2,23 @@
 # Inspired by https://github.com/myshov/dotfiles/blob/master/install/brew-cask.sh
 
 apps=(
+    1password
+    discord
+    spotify
+    slack
+    grammarly
+    zoom
+    obsidian
+    steam
+    shifty
+    notion
     firefox
     google-chrome
     keka
     visual-studio-code
     vlc
+    vanilla
+    iina
 )
 
 brew install "${apps[@]}" --cask
