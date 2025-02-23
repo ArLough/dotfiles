d166cc5014879bd2fd3843e805f8b87625ace2c8
Aidan Loughney <loughneyaidan@gmail.com>
2025-02-22 20:54:31 -0700

    cleanup pt2

--- brew/casks.sh
+++ brew/casks.sh
@@ -5,28 +5,14 @@ casks = (
 1password # Password Manager
 1password-cli
 obsidian # Notes/Second Brain
-# raindropio  # Bookmark manager
-# Entertainment
 spotify # Music
-steam # Gaming
-# Communication
-slack
 discord
 zoom
-# Browsers
-firefox
-google-chrome
-# Coding
 visual-studio-code
 iterm2 # Terminal
 jetbrains-toolbox # Jetbrains installer
 docker # Containerize for deployment
-# utm # VM
-# postman # API testing
-# Video Player
-vlc # Common Video Player
 iina # Mac-like Video Player
-# Utilities
 istat-menus # Computer stats in menu bar
 itsycal # Nice calendar in menu bar
 cheatsheet # CMD to pull up shortcuts
@@ -35,11 +21,6 @@ hiddenbar # Hide some menu bar items
 keka # Unarchiver
 rectangle # Window Manager
 authy # 2fa
-google-backup-and-sync # backup & sync
-# daisydisk # Disk Manager
-# Misc. applications
-brooklyn # Screensaver
-# Quicklook Plugins
 qlcolorcode
 qlstephen
 qlmarkdown
